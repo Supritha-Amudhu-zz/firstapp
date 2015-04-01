987a86962c6d303cfe514dd5430a5a72871a1f3d
Supritha-Amudhu <supritha.amudhu@gmail.com>
2015-04-01 12:56:01 +0530

    Version - April 1 - 3
    Sign up page linked.
    
    Signed-off-by: Supritha-Amudhu <supritha.amudhu@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
+      @title = "Sign up"
     end
   end
 
